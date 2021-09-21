9652b6a27e4d53d7310180141d3a1a04547efc83
Yura Bondarenko <ybondarenko.job@gmail.com>
2021-09-21 22:27:53 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="morse-code-translator2",
-    version="0.0.3",
+    version="1.0.0",
     description="Morse Code Translator",
     long_description=README,
     long_description_content_type="text/markdown",
